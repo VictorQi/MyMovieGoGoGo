8b7298a97565543650f35c53bbf368ea4977bfe9
Victor Qi <qijianqiong@gmail.com>
2017-03-22 15:25:17 +0800

    Initial Commit

---
+++ MyMoviesGoGoGo/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  MyMoviesGoGoGo
+//
+//  Created by v.q on 2017/3/22.
+//  Copyright © 2017年 Victor Qi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ MyMoviesGoGoGo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyMoviesGoGoGo
+//
+//  Created by v.q on 2017/3/22.
+//  Copyright © 2017年 Victor Qi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
